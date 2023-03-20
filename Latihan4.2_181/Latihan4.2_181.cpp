bc5921913b9be34cbc39626b9b9c504cd6668bb9
hi-lufz1 <latifufauzi@gmail.com>
2023-03-20 15:23:23 +0700

    tampilan isi array

--- Latihan4.2_181/Latihan4.2_181.cpp
+++ Latihan4.2_181/Latihan4.2_181.cpp
@@ -5,9 +5,16 @@ int main()
 {
     int a[5];
     cout << "Mengisi Array" << endl;
-    for (int i = 0; i<5; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << "Data ke- " << (i) << ":";
         cin >> a[i];
     }
+    cout << "Membaca Array" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Data ke- " << (i) << ":"<< a [i] << endl ;
+    }
 }
+
+
+
 
