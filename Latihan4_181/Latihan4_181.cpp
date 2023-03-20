6027cb240da138503392c679e30b38b2f2f1fe4e
hi-lufz1 <latifufauzi@gmail.com>
2023-03-20 15:13:22 +0700

    switch luas 1-4 dan loopingnya

--- Latihan4_181/Latihan4_181.cpp
+++ Latihan4_181/Latihan4_181.cpp
@@ -23,26 +23,35 @@ int main()
         cout << "2. Luas Lingkaran" <<endl;
         cout << "3. Luas Segitia" <<endl;
         cout << "4. Exit"<< endl;
-        cout << " Pilihan = " << endl;
+        cout << " Pilihan (1/2/3/4) = " << endl;
         cin >> pilihan;
 
         switch (pilihan )
         {
+        case 1:
+            cout << "Masukkan Panjang = " ;
+            cin >> panjang; 
+            cout << "Masukkan Lebar = ";
+            cin >> lebar;
+            cout << "Luas Persegi panjang = " << luaspersegi(panjang, lebar);
+            break;
+        case 2:
+            cout << "Masukkan jari jari = " ;
+            cin >> jejari;
+            cout << "Luas Lingkaran = " << luaslingkaran (jejari);
+            break;
+        case 3:
+            cout << "Masukkan Alas = " ;
+            cin >> alas;
+            cout << "Masukkan tinggi = " ;
+            cin >> tinggi;
+            cout << "Luas Segitiga = " << luassegitiga (alas, tinggi);
+            break;
+        case 4:
+            break;
         default:
+            cout << "pilihan salah" ;
             break;
         }
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-
+    } while (pilihan != 4);
 }
